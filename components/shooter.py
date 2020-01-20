1c2c328c29f403830e5a25087be62b76ec827f91
Injector-Spenral <sam.richards@thedropbears.org.au>
2020-01-20 19:13:32 +1100

    Simplified shooter.py component.

--- components/shooter.py
+++ components/shooter.py
@@ -7,17 +7,10 @@ from magicbot import tunable
 class Shooter:
     outer_motor: rev.CANSparkMax
     centre_motor: rev.CANSparkMax
-    loading_piston: wpilib.DoubleSolenoid
+    loading_piston: wpilib.Solenoid
 
-    # outer_rpm = tunable(0)
-    # centre_rpm = tunable(0)
-    def __init__(self):
-        self.outer_rpm = 4400
-        self.centre_rpm = -3700
-
-    def on_enable(self) -> None:
-        self.centre_motor.stopMotor()
-        self.outer_motor.stopMotor()
+    def __init__(self) -> None:
+        pass
 
     def setup(self) -> None:
         self.outer_motor.setIdleMode(rev.CANSparkMax.IdleMode.kCoast)
@@ -26,28 +19,19 @@ class Shooter:
         self.outer_encoder = self.outer_motor.getEncoder()
         self.centre_encoder = self.centre_motor.getEncoder()
 
-        self.centre_pid = self.centre_motor.getPIDController()
-        self.outer_pid = self.outer_motor.getPIDController()
+        self.outer_motor.setInverted(False)
+        self.centre_motor.setInverted(True)
 
-        for pid in (self.centre_pid, self.outer_pid):
-            pid.setP(5e-5)
-            pid.setI(1e-6)
-            pid.setD(0)
-            pid.setFF(0.000156)
+    def on_enable(self) -> None:
+        self.centre_motor.stopMotor()
+        self.outer_motor.stopMotor()
+        self.piston_down()
 
     def execute(self) -> None:
-        # self.outer_rpm = 0
-        # self.centre_rpm = 0
-        self.centre_pid.setReference(self.centre_rpm, rev.ControlType.kVelocity)
-        self.outer_pid.setReference(self.outer_rpm, rev.ControlType.kVelocity)
+        pass
 
-    def get_centre_error(self) -> float:
-        return self.centre_rpm - self.centre_encoder.getVelocity()
+    def piston_up(self) -> None:
+        self.loading_piston.set(True)
 
-    def get_outer_error(self) -> float:
-        return self.outer_rpm - self.outer_encoder.getVelocity()
-
-    def set_motor_rpm(self, v_outer, v_centre) -> None:
-        self.centre_rpm = v_centre
-        self.outer_rpm = v_outer
-
+    def piston_down(self) -> None:
+        self.loading_piston.set(False)
